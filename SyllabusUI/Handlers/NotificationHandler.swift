9c82bf1a8d6af61347387ded60dafc9ce5b3658b
Sadan Efe <69189063+kelboindaserver@users.noreply.github.com>
2023-08-04 16:36:51 +0300

    deneme

--- SyllabusUI/Handlers/NotificationHandler.swift
+++ SyllabusUI/Handlers/NotificationHandler.swift
@@ -18,7 +18,20 @@ class NotificationHandler {
             }
         }
     }
-    func sendNotification(date: Date,type:String,timeInterval: Double = 10 , title: String,body: String){
-        var trigger = UNNotificationTrigger?
+    func sendNotification (date: Date, type: String, timeInterval: Double = 10, title:String, body: String) {
+        var trigger: UNNotificationTrigger?
+        if type == "date" {
+            let dateComponents = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: date)
+            trigger = UNCalendarNotificationTrigger (dateMatching: dateComponents, repeats: false)
+        } else if type == "time" {
+            trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval,repeats: false)
+        }
+        let content = UNMutableNotificationContent ( )
+        content.title = title
+        content.body = body
+        content.sound = UNNotificationSound.default
+        let request = UNNotificationRequest(identifier: UUID() .uuidString, content:
+                                                content, trigger: trigger)
+        UNUserNotificationCenter.current () .add (request)
     }
 }
