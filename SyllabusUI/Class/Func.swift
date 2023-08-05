832a26bb0e1b865869efd649fea6153640e68a93
Sadan Efe <69189063+kelboindaserver@users.noreply.github.com>
2023-08-05 11:11:25 +0300

    notify

---
+++ SyllabusUI/Class/Func.swift
@@ -0,0 +1,8 @@
+//
+//  Func.swift
+//  SyllabusUI
+//
+//  Created by Şadan Efe Öz on 4.08.2023.
+//
+
+import Foundation
--- SyllabusUI/Handlers/NotificationHandler.swift
+++ SyllabusUI/Handlers/NotificationHandler.swift
@@ -20,18 +20,18 @@ class NotificationHandler {
     }
     func sendNotification (date: Date, type: String, timeInterval: Double = 10, title:String, body: String) {
         var trigger: UNNotificationTrigger?
-        if type == "date" {
+        if type == "exam" {
             let dateComponents = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: date)
             trigger = UNCalendarNotificationTrigger (dateMatching: dateComponents, repeats: false)
-        } else if type == "time" {
-            trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval,repeats: false)
+        } else if type == "lesson" {
+            let dateComponents = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: date)
+            trigger = UNCalendarNotificationTrigger (dateMatching: dateComponents, repeats: false)
         }
-        let content = UNMutableNotificationContent ( )
+        let content = UNMutableNotificationContent()
         content.title = title
         content.body = body
         content.sound = UNNotificationSound.default
-        let request = UNNotificationRequest(identifier: UUID() .uuidString, content:
-                                                content, trigger: trigger)
-        UNUserNotificationCenter.current () .add (request)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
     }
 }
